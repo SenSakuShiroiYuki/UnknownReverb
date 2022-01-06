f747b30fe9ee6dd48c41ad3cfbe5c14059cfcc88
SenSakuShiroiYuki <mark88323@gmail.com>
2022-01-06 19:17:03 +0800

    Update

---
+++ DearClione_2/Assets/Crest/Crest/Scripts/Time/TimeProviderBase.cs
@@ -0,0 +1,33 @@
+﻿// Crest Ocean System
+
+// Copyright 2020 Wave Harmonic Ltd
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Crest
+{
+    /// <summary>
+    /// Base class for scripts that provide the time to the ocean system. See derived classes for examples.
+    /// </summary>
+    public interface ITimeProvider
+    {
+        float CurrentTime { get; }
+        float DeltaTime { get; }
+
+        // Delta time used for dynamics such as the ripple sim
+        float DeltaTimeDynamics { get; }
+    }
+
+    public abstract class TimeProviderBase : MonoBehaviour, ITimeProvider
+    {
+        public abstract float CurrentTime { get; }
+        public abstract float DeltaTime { get; }
+        public abstract float DeltaTimeDynamics { get; }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(TimeProviderBase), editorForChildClasses: true), CanEditMultipleObjects]
+    class TimeProviderBaseEditor : Editor { }
+#endif
+}
